53a705ffe63884462be50a2a425bf96528161a02
IjaasMohamed <undyin0@gmail.com>
2023-02-07 07:40:19 -0300

    Commited front end

---
+++ src/components/create-exercise.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class CreateExercise extends Component {
+  render() {
+    return (
+      <div>
+        <p>you are on the Create Exercise Component</p>
+      </div>
+    );
+  }
+}
